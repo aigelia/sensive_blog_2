c98cfc2f89d34b748e7065088fe71d5e35300258
Katerina <e.mayakovskaya@gmail.com>
2025-07-17 06:38:31 +0300

    Move queries into QuerySet methods

--- blog/models.py
+++ blog/models.py
@@ -10,12 +10,33 @@ class PostQuerySet(models.QuerySet):
         posts_at_year = self.filter(published_at__year=year).order_by('published_at')
         return posts_at_year
 
+    def popular(self):
+        return self.annotate(likes_count=Count('likes')).order_by('-likes_count')
+
+    def fetch_with_comments_count(self):
+        """Добавляет к queryset количество комментариев к каждому посту.
+        Используется в случае, если к запросу уже применены другие аннотации.
+        Помогает избежать дублирования данных и ухудшения производительности."""
+        posts = self
+        posts_ids = [post.id for post in posts]
+        posts_with_comments = (
+            Post.objects
+            .filter(id__in=posts_ids)
+            .annotate(comments_count=Count('comments'))
+        )
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+
+        for post in posts:
+            post.comments_count = count_for_id[post.id]
+        return posts
+
 
 class TagQuerySet(models.QuerySet):
     def popular(self):
         popular_tags = (
-            self.
-            annotate(posts_with_tag=Count('posts'))
+            self
+            .annotate(posts_with_tag=Count('posts'))
             .order_by('-posts_with_tag')
             .prefetch_related('posts')
         )
--- blog/views.py
+++ blog/views.py
@@ -28,34 +28,19 @@ def serialize_tag(tag):
     }
 
 
-def get_necessary_posts(order_by_field, limit=None, tag=None):
-    posts = (
-        Post.objects
-        .annotate(likes_count=Count('likes'))
-        .select_related('author')
-        .prefetch_related('tags')
-    )
-    if tag:
-        posts = posts.filter(tags=tag)
-    posts = posts.order_by(order_by_field)
-
-    posts_ids = [post.id for post in posts]
-    posts_with_comments = (
-        Post.objects
-        .filter(id__in=posts_ids)
-        .annotate(comments_count=Count('comments'))
-    )
-    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
-    count_for_id = dict(ids_and_comments)
-
-    for post in posts:
-        post.comments_count = count_for_id[post.id]
-    return list(posts)[:limit] if limit else list(posts)
-
-
 def index(request):
-    most_popular_posts = get_necessary_posts('-likes_count', 5)
-    most_fresh_posts = get_necessary_posts('-published_at', 5)
+    most_popular_posts = (
+        Post.objects.popular()
+        .prefetch_related('author')[:5]
+        .fetch_with_comments_count()
+    )
+
+    most_fresh_posts = (
+        Post.objects
+        .select_related('author')
+        .prefetch_related('tags')[:5]
+        .fetch_with_comments_count()
+    )
 
     most_popular_tags = Tag.objects.popular()[:5]
 
@@ -114,17 +99,29 @@ def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5]
-    most_popular_posts = get_necessary_posts('-likes_count', limit=5)
 
-    related_posts = get_necessary_posts('-published_at', limit=20, tag=tag)
+    most_popular_posts = (
+        Post.objects
+        .popular()
+        .select_related('author')
+        .prefetch_related('tags')[:5]
+        .fetch_with_comments_count()
+    )
+
+    related_posts = (
+        Post.objects
+        .filter(tags=tag)
+        .order_by('-published_at')
+        .select_related('author')
+        .prefetch_related('tags')[:20]
+        .fetch_with_comments_count()
+    )
 
     context = {
         'tag': tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'posts': [serialize_post(post) for post in related_posts],
-        'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
-        ],
+        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
     }
     return render(request, 'posts-list.html', context)
 

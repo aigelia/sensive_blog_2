5029fbff7804fc7708aca941a7d2ce01d10ca676
Katerina <e.mayakovskaya@gmail.com>
2025-07-22 11:28:23 +0300

    Use .get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Comment, Post, Tag
 
 
@@ -54,12 +54,12 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = (
+    post = get_object_or_404(
         Post.objects
         .select_related('author')
         .prefetch_related('tags')
-        .annotate(likes_count=Count('likes'))
-        .get(slug=slug)
+        .annotate(likes_count=Count('likes')),
+        slug=slug
     )
     comments = (
         Comment.objects
@@ -109,7 +109,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
     most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = (

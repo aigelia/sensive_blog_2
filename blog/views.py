0511d17fb2a6edb94f3f01cf36ed56980f9bba74
Katerina <e.mayakovskaya@gmail.com>
2025-07-22 11:26:25 +0300

    Use .first instead of index

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from blog.models import Comment, Post, Tag
 
 
 def serialize_post(post):
+    tags = list(post.tags.all())
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -12,8 +13,8 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
+        'tags': [serialize_tag(tag) for tag in tags],
+        'first_tag_title': tags[0].title if tags else '',
     }
 
 
